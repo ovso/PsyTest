e3e067ea3c530cfda6accb4d12e641d9636e0b8d
jaeho <ovsoce@gmail.com>
2018-09-13 22:35:50 +0900

    Init ads

--- app/src/main/java/io/github/ovso/psytest/App.java
+++ app/src/main/java/io/github/ovso/psytest/App.java
@@ -11,6 +11,7 @@ public class App extends DaggerApplication {
 
     AppInitUtils.logger();
     AppInitUtils.crashlytics(this);
+    AppInitUtils.ads(this);
   }
 
   @Override protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
--- app/src/main/java/io/github/ovso/psytest/AppInitUtils.java
+++ app/src/main/java/io/github/ovso/psytest/AppInitUtils.java
@@ -3,6 +3,7 @@ package io.github.ovso.psytest;
 import android.content.Context;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.core.CrashlyticsCore;
+import com.google.android.gms.ads.MobileAds;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
 import io.fabric.sdk.android.Fabric;
@@ -20,5 +21,9 @@ public final class AppInitUtils {
   public static void crashlytics(Context context) {
     CrashlyticsCore core = new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build();
     Fabric.with(context, new Crashlytics.Builder().core(core).build());
+  }
+
+  public static void ads(Context context) {
+    MobileAds.initialize(context, Security.ADMOB_APP_ID.getValue());
   }
 }
