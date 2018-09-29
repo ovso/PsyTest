bf3df47f6300781d5a601fc354a563ad16d208b4
jaeho <ovsoce@gmail.com>
2018-09-29 15:02:35 +0900

    Versioning

--- app/src/main/java/io/github/ovso/psytest/ui/main/MainActivity.java
+++ app/src/main/java/io/github/ovso/psytest/ui/main/MainActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import butterknife.BindView;
