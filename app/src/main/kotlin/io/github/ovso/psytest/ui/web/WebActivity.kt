20f47ca513b80d52a3305f5b11cca93c7161bfc5
jaeho <ovsoce@gmail.com>
2018-12-20 04:48:39 +0900

    Edit web

--- app/src/main/kotlin/io/github/ovso/psytest/ui/web/WebActivity.kt
+++ app/src/main/kotlin/io/github/ovso/psytest/ui/web/WebActivity.kt
@@ -1,16 +1,26 @@
 package io.github.ovso.psytest.ui.web
 
+import android.content.Intent
+import android.content.res.ColorStateList
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.MenuItem
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import butterknife.OnClick
 import io.github.ovso.psytest.R
 import io.github.ovso.psytest.ui.base.view.BaseActivity
 import kotlinx.android.synthetic.main.content_web.progressbar_web
 import kotlinx.android.synthetic.main.content_web.webview_web
+import kotlinx.android.synthetic.main.navigation_web.button_web_back
+import kotlinx.android.synthetic.main.navigation_web.button_web_browser
+import kotlinx.android.synthetic.main.navigation_web.button_web_forward
+import kotlinx.android.synthetic.main.navigation_web.button_web_refresh
+import kotlinx.android.synthetic.main.navigation_web.button_web_share
 
 class WebActivity : BaseActivity() {
   override fun getLayoutResID(): Int {
@@ -21,6 +31,7 @@ class WebActivity : BaseActivity() {
     super.onCreate(savedInstanceState)
     setupActionBar()
     setupWebView()
+    setupNaviEvent()
     webview_web.loadUrl(intent.getStringExtra("url"));
   }
 
@@ -42,10 +53,8 @@ class WebActivity : BaseActivity() {
     settings.setGeolocationEnabled(true)
     settings.cacheMode = WebSettings.LOAD_NO_CACHE
 
-    webview_web.webChromeClient = WebChromeClient()
-    webview_web.webViewClient = WebViewClient()
-    //webview_web.webChromeClient
-
+    webview_web.webChromeClient = MyWebChromeClient()
+    webview_web.webViewClient = MyWebViewClient()
   }
 
   private inner class MyWebChromeClient : WebChromeClient() {
@@ -63,23 +72,87 @@ class WebActivity : BaseActivity() {
     override fun onPageStarted(
       view: WebView,
       url: String,
-      favicon: Bitmap
+      favicon: Bitmap?
     ) {
       progressbar_web.show()
       super.onPageStarted(view, url, favicon)
     }
 
     override fun onPageFinished(
-      view: WebView,
-      url: String
+      view: WebView?,
+      url: String?
     ) {
       progressbar_web.hide()
-      //updateWebNaviButton()
+      updateWebNaviButton()
       super.onPageFinished(view, url)
     }
   }
 
   override fun isTitle(): Boolean {
     return true
+  }
+
+  private fun updateWebNaviButton() {
+    if (webview_web.canGoBack()) {
+      val color = ContextCompat.getColor(this, android.R.color.black)
+      button_web_back.setImageTintList(ColorStateList.valueOf(color))
+      button_web_back.setClickable(true)
+    } else {
+      val color = ContextCompat.getColor(this, android.R.color.darker_gray)
+      button_web_back.setImageTintList(ColorStateList.valueOf(color))
+      button_web_back.setClickable(false)
+    }
+
+    if (webview_web.canGoForward()) {
+      val color = ContextCompat.getColor(this, android.R.color.black)
+      button_web_forward.setImageTintList(ColorStateList.valueOf(color))
+      button_web_forward.setClickable(true)
+    } else {
+      val color = ContextCompat.getColor(this, android.R.color.darker_gray)
+      button_web_forward.setImageTintList(ColorStateList.valueOf(color))
+      button_web_forward.setClickable(false)
+    }
+  }
+
+  private fun setupNaviEvent() {
+    button_web_back.setOnClickListener {
+      if (webview_web.canGoBack()) {
+        webview_web.goBack()
+      }
+    }
+    button_web_forward.setOnClickListener {
+      if (webview_web.canGoForward()) {
+        webview_web.goForward()
+      }
+    }
+    button_web_refresh.setOnClickListener { webview_web.reload() }
+    button_web_share.setOnClickListener { navigateToShare() }
+    button_web_browser.setOnClickListener { navigateToBrowser() }
+  }
+
+  fun navigateToBrowser() {
+    val intent = Intent(Intent.ACTION_VIEW)
+    intent.data = Uri.parse(webview_web.getUrl())
+    startActivity(intent)
+  }
+
+  fun navigateToShare() {
+    val intent = Intent()
+    intent.action = Intent.ACTION_SEND
+    intent.putExtra(Intent.EXTRA_TEXT, webview_web.getUrl())
+    intent.type = "text/plain"
+    startActivity(Intent.createChooser(intent, resources.getString(R.string.web_share)))
+  }
+
+  override fun onStop() {
+    webview_web.onPause()
+    webview_web.pauseTimers()
+    super.onStop()
+  }
+
+  override fun onResume() {
+    webview_web.onResume()
+    webview_web.resumeTimers()
+    super.onResume()
   }
 }
