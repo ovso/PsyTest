eb7130880ff4dcd69af299e8b6f4d918fc60bf5a
jaeho <ovsoce@gmail.com>
2019-01-02 23:44:10 +0900

    Convert java to kotlin

---
+++ app/src/main/java/io/github/ovso/psytest/data/network/SearchRequest.kt
@@ -0,0 +1,47 @@
+package io.github.ovso.psytest.data.network
+
+import android.text.TextUtils
+import io.github.ovso.psytest.Security
+import io.github.ovso.psytest.data.KeyName
+import io.github.ovso.psytest.data.network.model.Search
+import io.reactivex.Single
+import java.util.HashMap
+import javax.inject.Inject
+import okhttp3.Headers
+
+class SearchRequest @Inject constructor() : BaseRequest<SearchService>() {
+  override val baseUrl: String
+    get() = ApiEndPoint.SEARCH.url
+  override val apiClass: Class<SearchService>
+    get() = SearchService::class.java
+
+  override fun createHeaders(): Headers {
+    return Headers.Builder()
+        .build()
+  }
+
+  fun getResult(
+    q: String?,
+    pageToken: String?
+  ): Single<Search> {
+    return api.getResult(createQueryMap(q!!, pageToken))
+  }
+
+  private fun createQueryMap(
+    q: String,
+    pageToken: String?
+  ): Map<String, Any> {
+    val queryMap = HashMap<String, Any>()
+    queryMap[KeyName.Q.get()] = q
+    queryMap[KeyName.MAX_RESULTS.get()] = 50
+    queryMap[KeyName.ORDER.get()] = "viewCount"
+    queryMap[KeyName.TYPE.get()] = "video"
+    queryMap[KeyName.VIDEO_SYNDICATED.get()] = "any"
+    queryMap[KeyName.KEY.get()] = Security.KEY.value
+    queryMap[KeyName.PART.get()] = "snippet"
+    if (!TextUtils.isEmpty(pageToken)) {
+      queryMap[KeyName.PAGE_TOKEN.get()] = pageToken!!
+    }
+    return queryMap
+  }
+}
