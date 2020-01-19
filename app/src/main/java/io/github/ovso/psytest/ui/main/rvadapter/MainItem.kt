8fb46da7951e280e60f6f651c6e74ffea1785600
peterjaeho <peter@mybroccoli.co.kr>
2020-01-19 21:39:09 +0900

    Imple click on MainItem

--- app/src/main/java/io/github/ovso/psytest/ui/main/rvadapter/MainItem.kt
+++ app/src/main/java/io/github/ovso/psytest/ui/main/rvadapter/MainItem.kt
@@ -1,6 +1,13 @@
 package io.github.ovso.psytest.ui.main.rvadapter
 
+import android.view.View
+
 data class MainItem(
   val title: String = "",
   val query: String = ""
-)
+) {
+
+  fun onClick(v: View) {
+
+  }
+}
